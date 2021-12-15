429dead1b1f5c435e89a6932a4b6a199097e8ece
Raytroll <80167098+Raytroll@users.noreply.github.com>
2021-12-15 14:01:48 +0100

    Update Download ffmpeg tools from Google Drive.bat

--- Download ffmpeg tools from Google Drive.bat
+++ Download ffmpeg tools from Google Drive.bat
@@ -2,13 +2,15 @@
 title Download : ffmpeg tools batch
 if exist install.bat (echo Programme deja telecharger !
 goto:choix)
-goto:wait
+goto:install
 :choix
 echo 1:oui
 echo 2:non
 set /p choix=voulez vous mettre a jour:
 if %choix% == 1 (goto:wait)
 if %choix% == 2 (exit)
+
+:install
 start https://drive.google.com/drive/u/0/folders/1vc6ixwv0198Cq1eMO_ITXKyTkt6ZTcTn
 echo Waiting for file : install.zip ...
 
